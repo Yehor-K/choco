5ee41b3df63a1eda8c2ea5133f1b7a3adb6158a7
Yehor-K <yehorkuch@gmail.com>
2021-04-15 00:50:05 +0300

    footer verstka

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import telegram from "../../img/telegram.png";
+import whatsapp from "../../img/whatsapp.png";
+import viber from "../../img/viber.png";
+import instagram from "../../img/instagram.png";
 
 function Footer() {
   return (
-    <footer className="footer">
+    <footer>
       <div className={styles.footer}>
         <div className={styles.contacts}>
           <p>Свяжитесь с нами:</p>
           <div className={styles.number}>
+            <img src={telegram} alt="telegram" />
+            <img src={whatsapp} alt="whatsapp" />
+            <img src={viber} alt="viber" />
             <span>+380000000000</span>
           </div>
+        </div>
+        <div className={styles.instagram}>
+          <p>Следите за нами в Instagram: </p>
+          <a href="https://www.instagram.com/chocoharmoni/"><img src={instagram} alt="instagram" /></a>
         </div>
       </div>
     </footer>
